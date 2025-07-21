3c87f7bf520c143201af56214907e5bb8879e8c3
ruzul2185 <ruzulchaure2185@gmail.com>
2025-07-21 22:57:36 +0530

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 // Regitering plugins in App file make them accessable globally
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
