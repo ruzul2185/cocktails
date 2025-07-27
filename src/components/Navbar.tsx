5bc3eeb94a2ae0ee869e4a9cf04a7652c9d9000d
ruzul2185 <ruzulchaure2185@gmail.com>
2025-07-27 13:09:52 +0530

    done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       <div>
         <a href="#home" className="flex items-center gap-2">
           <img src="/images/logo.png" alt="logo" />
-          <p>Velvet Pour</p>
+          <p>Chronic Alcholic</p>
         </a>
 
         <ul>
