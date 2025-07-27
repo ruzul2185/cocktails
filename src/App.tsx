a6c672f44019f06df3d859db9bc72ff4a63a53de
ruzul2185 <ruzulchaure2185@gmail.com>
2025-07-27 11:43:44 +0530

    about done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 // Regitering plugins in App file make them accessable globally
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
