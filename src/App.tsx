dff493d94d369f5da0dec060dc5958246cc671e5
ruzul2185 <ruzulchaure2185@gmail.com>
2025-07-27 13:08:25 +0530

    Contact done

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 // Regitering plugins in App file make them accessable globally
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -19,6 +20,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
+      <Contact />
     </main>
   );
 };
---
+++ src/components/Contact.tsx
@@ -0,0 +1,95 @@
+import { useGSAP } from "@gsap/react";
+import { socials } from "../constants.tsx";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
+const Contact = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", {
+      type: "words",
+    });
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+
+    timeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", {
+        y: "-50",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to(
+        "#f-left-leaf",
+        {
+          y: "-50",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
+  return (
+    <footer id="contact">
+      <img
+        src="/images/footer-right-leaf.png"
+        alt="leaf-right"
+        id="f-right-leaf"
+      />
+      <img
+        src="/images/footer-left-leaf.png"
+        alt="leaf-left"
+        id="f-left-leaf"
+      />
+
+      <div className="content">
+        <h2>Where to Find Us</h2>
+
+        <div>
+          <h3>Creator</h3>
+          <p>ruzul2185</p>
+        </div>
+
+        <div>
+          <h3>Phone</h3>
+          <p>+91 ***-***-****</p>
+          <p>ruzulchaure2185@gmail.com</p>
+        </div>
+
+        <div>
+          <h3>Socials</h3>
+
+          <div className="flex-center gap-5">
+            {socials.map((social) => (
+              <a
+                href={social.url}
+                key={social.name}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={social.name}
+              >
+                <img src={social.icon} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
