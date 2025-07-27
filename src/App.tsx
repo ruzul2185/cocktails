bb960c6e8c85d2bd0a9f365779670b947c9330a8
ruzul2185 <ruzulchaure2185@gmail.com>
2025-07-27 12:01:52 +0530

    art completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 // Regitering plugins in App file make them accessable globally
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -15,6 +16,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 };
